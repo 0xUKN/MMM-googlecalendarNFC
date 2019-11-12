7e89a15d93dbb56a34325a8069f87b8c41767db1
0xUKN <0xukn@protonmail.com>
2019-11-12 22:56:02 +0100

    Disable auth token storage

--- calendarfetcher.js
+++ calendarfetcher.js
@@ -18,10 +18,8 @@ var moment = require('moment'),
 
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'],
     CALENDAR_IDS = ['primary'],
-    TOKEN_DIR = __dirname + '/.credentials/',
     CONFIG_DIR = __dirname + '/config/',
     GOOGLE_API_CONFIG_PATH = CONFIG_DIR + 'credentials.json',
-    TOKEN_PATH = TOKEN_DIR + 'calendar-credentials.json';
 
 var CalendarFetcher = function(calendarName, reloadInterval, maximumEntries, maximumNumberOfDays) {
     var self = this;
@@ -41,7 +39,6 @@ var CalendarFetcher = function(calendarName, reloadInterval, maximumEntries, max
 
         // Load client secrets from a local file.
         try {
-            //const content = fs.readFileSync(TOKEN_PATH);
             const content = fs.readFileSync(GOOGLE_API_CONFIG_PATH);
             authorize(JSON.parse(content), listEvents);
         } catch (err) {
@@ -99,15 +96,7 @@ var CalendarFetcher = function(calendarName, reloadInterval, maximumEntries, max
         // console.log("MMM-googlecalendar: ", client_secret, client_id, redirect_uris);
 
         const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-        // Check if we have previously stored a token.
-        try {
-            token = fs.readFileSync(TOKEN_PATH);
-          } catch (err) {
-            return getNewToken(oAuth2Client, callback);
-          }
-          oAuth2Client.setCredentials(JSON.parse(token));
-          callback(oAuth2Client);
+        return getNewToken(oAuth2Client, callback);
     };
 
     /**
@@ -236,23 +225,6 @@ var CalendarFetcher = function(calendarName, reloadInterval, maximumEntries, max
         });
     };
 
-
-    /**
-     * Store token to disk be used in later program executions.
-     *
-     * @param {Object} token The token to store to disk.
-     */
-    var storeToken = function(token) {
-        try {
-            fs.mkdirSync(TOKEN_DIR);
-        } catch (err) {
-            if (err.code != 'EEXIST')
-                throw err;
-        }
-
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    };
-
     /**
      * Get and store new token after prompting for user authorization, and then
      * execute the given callback with the authorized OAuth2 client.
@@ -284,13 +256,6 @@ var CalendarFetcher = function(calendarName, reloadInterval, maximumEntries, max
             oAuth2Client.getToken(code, (err, token) => {
                 if (err) return callback(err);
                 oAuth2Client.setCredentials(token);
-                // Store the token to disk for later program executions
-                try {
-                    fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
-                    console.log('Token stored to', TOKEN_PATH);
-                } catch (err) {
-                    console.error(err);
-                }
                 callback(oAuth2Client);
             });
         });
